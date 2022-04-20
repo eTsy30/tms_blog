3f79ab6c3eea297ca1a3f2b37e9dc023fab1eae7
Evgen Tsyhankov <92335859+eTsy30@users.noreply.github.com>
2022-04-20 16:01:54 +0300

    fit: update App.tsx
    fit: update App.tsx

--- tms_blog/src/App.tsx
+++ tms_blog/src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
+import{Button} from './components/Button/Button'
+
+import bBwi from './components/image/BookmarkButtonControlBlack.png'
+import gBwi from './components/image/gBwi.png'
+import wBwi from './components/image/bwiW.png'
+import dis from './components/image/dislike.png'
+import like from './components/image/wlike.png'
+function logg():void{
+  return console.log('123',new Date());
+  
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <div className='App'>
+          <Button 
+          text='Primary' 
+          
+          className='primary' 
+          onClick={logg} 
+          />
+
+          <Button 
+          text='Secondary' 
+          className='secondary' 
+          onClick={logg} 
+          />
+
+          <Button 
+          text='Secondary 2' 
+          className='secondary2' 
+          onClick={logg} 
+          />
+          <Button text=' Button with icon' className='ButtonWithIcon' onClick={logg} icon={bBwi}/>
+          <Button  disabled={false} className='like' onClick={logg}  icon={dis}/>
+          <Button  disabled={false} className='dislike' onClick={logg}  icon={like}/>
+
+  </div>
+  
+
   );
 }
 
