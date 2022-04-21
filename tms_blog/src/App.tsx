65a68ce91f8969de38994b8fd6ca9a25f96bdb79
Evgen Tsyhankov <92335859+eTsy30@users.noreply.github.com>
2022-04-21 23:39:51 +0300

    fit: add component input

--- tms_blog/src/App.tsx
+++ tms_blog/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="App">
@@ -20,6 +21,34 @@ function App() {
         </a>
       </header>
     </div>
+=======
+import{Button} from './components/Button/Button'
+import {Input} from './components/Inputs/Input'
+import {NameForm} from './components/Inputs/Input'
+import bBwi from './components/image/11.png'
+import dis from './components/image/33.png'
+import like from './components/image/22.png'
+function logg():void{
+  return console.log('123',new Date());
+  
+}
+
+function App() {
+  return ( <div className='App'>
+    <Button disabled text='Primary' className='primary' onClick={logg} />
+    <Button text='Secondary' className='secondary'onClick={logg} />
+    <Button  text='Secondary 2' className='secondary2' onClick={logg} />
+    <Button text=' Button with icon' className='ButtonWithIcon' onClick={logg} icon={bBwi}/>
+    <Button  disabled className='like' onClick={logg}  icon={dis}/>
+    <Button  disabled className='dislike' onClick={logg}  icon={like}/>
+    {/* <Input text='Title' value='Placeholder' className='defaultImp'/> 
+    <Input text='Title'   className='defaultImp'/>  */}
+    <NameForm  placeholder='some1' className='defaultImp'/>
+    <NameForm  placeholder='some2' className='defaultImp' disabled/>
+  </div>
+  
+
+>>>>>>> Stashed changes
   );
 }
 
---
+++ tms_blog/src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { log } from 'console'
+import React from 'react'
+import'./Button.css'
+import clsx from 'clsx'
+type Bprops ={
+    text?: string,
+    onClick: ()=>void,
+    className:string,
+    disabled?: boolean,
+    icon?: string | undefined,
+}
+ export const Button = (props:Bprops)=>{
+    
+     return(
+     <button type='button' 
+     
+        disabled={props.disabled} 
+        className={clsx(props.className,'ourButtonStyle',{
+            'buttonDisable':props.disabled}
+            )}
+        onClick={props.onClick}>
+      {props.icon  && <img  className='buttonImg' src={props.icon} />}
+         {props.text}
+    </button>)
+ }
+ 
