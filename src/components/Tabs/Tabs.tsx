a833c1084c04f564c2b5e28c0328253e070aa2a7
tsy <tsy@mail.com>
2022-05-16 13:56:36 +0300

    fix: component TAbComponent

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,40 +1,30 @@
 import { log } from "console";
 import { type } from "os";
 import React, { useState } from "react";
-import './Tabs.css'
-type TybeProps={
-  title:string, 
-  content:any 
-}
-const TabContent = ({ title, content }:TybeProps) => (
-  <div className="Tab__block--Contetn">
-    <h3>{title}</h3>
-    <div>{content}</div> 
-  </div>
-);
+import "./Tabs.css";
+import { TabContent } from "./TabContent/TabContent";
 
-export const Tabs =({items}:any)=>{
-  const [active,setActive]=useState(0)
-  const openTab = (e:any)=> setActive(+e.target.dataset.index);
+export const Tabs = ({ items }: any) => {
+  const [active, setActive] = useState(0);
+  const openTab = (e: any) => setActive(+e.target.dataset.index);
 
-
-  
-    return(
-      <div>
-        <div className="Tabs__wrapper">
-          {items.map((el:any,index:number)=>(
- 
-            <button
-            
-            className={`Tab__block  ${index === active ? 'Tab__block__active' : console.log(index, active )}`}
-              onClick={openTab}
-           
+  return (
+    <div>
+      <div className="Tabs__wrapper">
+        {items.map((el: any, index: number) => (
+          <button
+            className={`Tab__block  ${
+              index === active ? "Tab__block__active" : ""
+            } `}
+            onClick={openTab}
             data-index={index}
-          >{el.title}</button>
-          ))}
-        </div>  
-        
-    { items[Number(active)] &&  <TabContent {...items[Number(active)]}/>} 
+          >
+            {el.title}
+          </button>
+        ))}
+      </div>
+
+      {items[Number(active)] && <TabContent {...items[Number(active)]} />}
     </div>
-  )
-}
+  );
+};
