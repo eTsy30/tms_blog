92c851f919ed70ce7ef8caf06e99b5e6893ae047
tsy <tsy@mail.com>
2022-05-16 20:22:11 +0300

    fix:add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Tabs } from './components/Tabs';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Tabs } from "./components/Tabs";
+import { useState } from "react";
+import { setTheme } from "./components/redux/theme";
 
 const items = [
-  { title: 'All', content: 'All content' },
-  { title: 'My favorites', content: ' My favorites content' },
-  { title: 'Popular', content: ' Popular content' },
+  { title: "All", content: "All content" },
+  { title: "My favorites", content: " My favorites content" },
+  { title: "Popular", content: " Popular content" },
 ];
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
+    <div className={`App theme--${theme}`}>
+      <Tabs items={items} onClick={toggleTheme} />
 
-
-<Tabs items={items} />
+      <button onClick={toggleTheme}>dfhfghfghfg</button>
     </div>
   );
 }
--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from './theme'
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        theme: themeReducer
+    }
 })
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
